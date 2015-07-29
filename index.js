4f1aea80e2435b6c25a52123332ac3aea1ad2dbf
SebastienLouis <sebastien.louis.pro@gmail.com>
2015-07-29 14:30:43 +0200

    update

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 var express = require('express');
 var app = express();
 
+
 app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':3002');
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3002');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        next();
+    }
+);
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 	app.get('/', function(request, response) {
 
 	 var user ={
