0cfe163701bc94d9c59be2a07f5261f31876090a
SebastienLouis <sebastien.louis.pro@gmail.com>
2015-07-29 14:31:48 +0200

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 
 app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':3002');
+        res.setHeader('Access-Control-Allow-Origin', "http://localhost:3002 ");
 
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
