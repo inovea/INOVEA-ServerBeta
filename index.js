fbd30581f5fde682d0ef9f8defc051c5a74ddb93
SebastienLouis <sebastien.louis.pro@gmail.com>
2015-07-29 14:09:34 +0200

    update

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.get('/', function(req, res){
+	res.send("Hello world from server.js");
+});
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.send('Server is running');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/getUser', function(req, res){
+
+	alert('getUser');
+	var user ={
+		name : "louis",
+		prenom : "sebastien",
+		age : "20"
+	}
+	res.json(user);
 });
-
-
