59e94d7d06133f1812ce16c56054ef77bd9bba24
Bentalha Oussama <oussama445@hotmail.com>
2015-08-02 11:49:33 +0200

    Webservices : User & Errand

--- index.js
+++ index.js
@@ -32,9 +32,8 @@ app.use(function (req, res, next) {
 //Connection to database
 mongoose.connect("mongodb://admin:admin@ds053708.mongolab.com:53708/heroku_c37tjzdd");
 
-var User = mongoose.model('users', {name : String, firstname : String});
-
-
+var User = mongoose.model('users', { _id = <idUser>, name : String, firstname : String, mail : String, password : String, admin : Boolean});
+var Errand = mongoose.model('errands', {state : int, dateDebut : Date, dateFin : Date, duree : double, distance : double, user_id : <idUser> });
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -59,7 +58,7 @@ var User = mongoose.model('users', {name : String, firstname : String});
   */
   app.post('/users', function(req, res){  
     console.dir('[addUser] called');
-    var newUser = new User({ name : req.body.name, firstname : req.body.firstname });
+    var newUser = new User({name : req.body.name, firstname : req.body.firstname, mail : req.body.mail, password : req.body.password, admin : req.body.admin});
     newUser.save(function (err, result) {
       if (err) return handleError(err);
 
@@ -100,6 +99,73 @@ var User = mongoose.model('users', {name : String, firstname : String});
   });
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// END USER WEBSERVICES //////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// ERRAND WEBSERVICES ////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/* 
+  Get errands
+  */
+  app.get('/errands', function (req, res) {
+  // Connect to the db
+  mongoose.model('errands').find(function(err, errands){
+    console.dir('[getErrands] called');
+    res.send(errands);
+  })      
+
+});
+
+
+/* 
+  Add errand
+  */
+  app.post('/errands', function(req, res){  
+    console.dir('[addErrand] called');
+    var newErrand = new Errand({state : req.body.state, dateDebut : req.body.dateDebut, dateFin : req.body.dateFin, duree : req.body.duree, distance : req.body.distance, user_id : req.body.user_id });
+    newErrand.save(function (err, result) {
+      if (err) return handleError(err);
+
+      console.log(result);
+      res.end();
+    });
+  });
+
+/*
+  Delete errand 
+  */
+  app.post('/deleteErrand', function (req, res) {
+    Errand.findById(req.body.id, function (err, user) {
+      errand.remove(function (err, removedErrand) {
+        if (err) return handleError(err);
+        console.log('Object deleted !');
+
+        res.end();
+      })
+    })
+  });
+
+/* 
+  Update errand 
+  */
+  app.post('/updateErrand', function (req, res) {
+    console.dir('[updateErrand] called');
+
+
+    Errand.findById(req.body.id, function (err, errand) {
+      if(err)
+        res.send(err);
+      // Model.update (user found by id, new user, null, function)
+      Errand.update(errand, req.body, null, function(error, result){
+        res.end();
+      })
+    })
+  });
+
+
 
 //Select the port 8080 if the serve is launch in local
 var port = process.env.PORT || 8080;
@@ -112,5 +178,5 @@ var server = app.listen(port, function () {
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////// END USER WEBSERVICES //////////////////////////////////////////
+//////////////////////////////// END ERRAND WEBSERVICES //////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
